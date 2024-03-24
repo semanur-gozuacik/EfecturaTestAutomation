0b9dc0dc261cd6841344610ad736f74087e519c5
semanur-gozuacik <144659195+semanur-gozuacik@users.noreply.github.com>
2024-03-24 05:45:45 +0300

    Update Hooks.java

--- src/test/java/com/sema/stepDefs/Hooks.java
+++ src/test/java/com/sema/stepDefs/Hooks.java
@@ -17,7 +17,8 @@ import java.sql.SQLException;
 
 public class Hooks {
     private static final Logger logger = LoggerFactory.getLogger(Hooks.class);
-
+    private static long startTime;
+    private static final long MAX_DURATION_IN_MILLIS = 180000;
     @Before
     public void setup(Scenario scenario) {
         System.setProperty("webdriver.http.factory", "jdk-http-client");
@@ -27,9 +28,19 @@ public class Hooks {
         logger.info("Test scenario :: {}", scenario.getName());
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        startTime = System.nanoTime();
+
     }
     @After
     public static void tearDown(Scenario scenario) {
+        long endTime = System.nanoTime();
+        long durationInNanos = endTime - startTime;
+        long durationInMillis = durationInNanos / 1000000;
+        if (durationInMillis > MAX_DURATION_IN_MILLIS) {
+            scenario.log("Scenario took too long. Failing the scenario.");
+            Driver.closeDriver();
+            throw new RuntimeException("Scenario took too long.");
+        }
         if (scenario.isFailed()) {
             TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
             byte[] image = takesScreenshot.getScreenshotAs(OutputType.BYTES);
@@ -39,5 +50,12 @@ public class Hooks {
             logger.info("Test scenario :: {} :: Passed", scenario.getName());
         }
         Driver.closeDriver();
+
+        try {
+           Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
