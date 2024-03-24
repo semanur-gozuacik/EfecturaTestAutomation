0829d5a83a3f453d68432260dc3887e0d68c07e8
Fatih KARA <155437456+fatihkara67@users.noreply.github.com>
2024-03-24 07:19:38 +0300

    Update Driver.java

--- src/test/java/com/sema/utilities/Driver.java
+++ src/test/java/com/sema/utilities/Driver.java
@@ -38,6 +38,7 @@ public class Driver {
             String browser = ConfigurationReader.getProperty("browser").toLowerCase();
             switch (browser) {
                 case "chrome":
+                    WebDriverManager.chromedriver().setup();
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--remote-allow-origins=*");
                     chromeOptions.addArguments("--start-maximized");
