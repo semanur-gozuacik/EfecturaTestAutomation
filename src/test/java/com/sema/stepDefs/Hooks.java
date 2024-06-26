2846f9dfa32d6848cc5c9170f1472bbe0c63ba86
semanur-gozuacik <semanur.gozuacik@efectura.com>
2024-06-26 20:03:25 +0300

    ..

--- src/test/java/com/sema/stepDefs/Hooks.java
+++ src/test/java/com/sema/stepDefs/Hooks.java
@@ -41,7 +41,7 @@ public class Hooks {
         Driver.closeDriver();
 
         try {
-           Thread.sleep(6000);
+           Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
