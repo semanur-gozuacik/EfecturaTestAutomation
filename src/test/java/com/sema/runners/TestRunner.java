efbf67b4628b7c857770bcdcaa2aaef850f38ffd
semanur-gozuacik <semanur.gozuacik@efectura.com>
2024-07-28 21:25:35 +0300

    test

--- src/test/java/com/sema/runners/TestRunner.java
+++ src/test/java/com/sema/runners/TestRunner.java
@@ -24,7 +24,7 @@ import java.util.List;
                 "rerun:target/rerun.txt"
         },
         // Hangi senaryoların çalıştırılacağını belirten etiket
-        tags ="@regression",
+        tags ="@testtttt",
         features ="src/test/java/com/sema/features",
         glue  = "com/sema/stepDefs",
         dryRun = false
